e6ac06ca3b87fba4f349927e8cb9fb1f061cdcc5
DESKTOP-NLSQ1U9\user <hello2263@naver.com>
2021-05-10 15:06:42 +0900

    배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
    예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
    array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
    1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
    2에서 나온 배열의 3번째 숫자는 5입니다.
    배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.

---
+++ test_0506_1.py
@@ -0,0 +1,15 @@
+def solution(array, commands):
+    answer = []
+    k = []
+    for i in commands:
+        for j in i:
+            k.append(j)
+        a = array[k[0]-1:k[1]]
+        a = sorted(a)
+        answer.append(a[(k[2]-1)])
+        k = []
+    return answer
+
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+print(solution(array, commands))
