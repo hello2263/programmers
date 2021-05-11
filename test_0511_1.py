da5dfc47f925492bd30fde78ea05e39e9881e67c
DESKTOP-NLSQ1U9\user <hello2263@naver.com>
2021-05-11 16:03:50 +0900

    프로그래머스 해시 > 전화번호 목록
    미완성

--- test_0511_1.py
+++ test_0511_1.py
@@ -1,28 +1,31 @@
-
+#
+# def solution(phone_book):
+#     answer = True
+#     phone_book = sorted(phone_book, key=len)
+#     for i in range(0,len(phone_book)):
+#         min_num = phone_book[i]
+#         phone = [h[:len(min_num)] for h in phone_book]
+#         phone.pop(i)
+#         if min_num in phone:
+#             answer = False
+#             break
+#     return answer
+#
 def solution(phone_book):
     answer = True
-    phone_book = sorted(phone_book, key=len)
-    for i in range(0,len(phone_book)):
-        min_num = phone_book[i]
-        phone = [h[:len(min_num)] for h in phone_book]
-        phone.pop(i)
-        if min_num in phone:
-            answer = False
-            break
+    phone_book = sorted(phone_book)
+    count = 0
+    for h in phone_book:
+        min_num = h
+        phone = h[:len(min_num)]
+        if (phone == min_num ):
+            count = count + 1
+            if (count > 1):
+                answer = False
     return answer
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+phone_book = ["123", "456", "789"]
+print(solution(phone_book))
+phone_book = ["119", "97674223", "1195524421"]
+print(solution(phone_book))
