c0b87fd013ab5b3d1a7563057f05bb55bcc397b2
DESKTOP-NLSQ1U9\user <hello2263@naver.com>
2021-05-13 15:40:34 +0900

    프로그래머스 힙 > 더 맵게
    미완성

--- test_0513_1.py
+++ test_0513_1.py
@@ -17,3 +17,19 @@ scoville = [1, 2, 3, 9, 10, 12]
 goal = 0
 k = 7
 print(solution(scoville, k))
+
+
+
+"""
+heapify : 기존의 리스트를 힙으로 변환
+
+heappop : 힙에서 원소 삭제. 원소를 삭제할 대상 리스트를 인자로 넘기면, 가장 작은 원소를 삭제 후에 그 값을 리턴한다.
+
+ex) heap = [1, 3, 7, 4]
+
+print(heapq.heappop(heap)) -->1
+
+print(heap)  -->[3, 4, 7]
+
+heappush : 힙에 원소 추가
+"""
