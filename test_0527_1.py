d4a0e172cb6700a2e0013f565e95795f01e02919
Dogyu <hello2263@naver.com>
2021-05-27 15:54:29 +0900

    프로그래머스 힙 > 이중순위스택큐
    이중 우선순위 큐는 다음 연산을 할 수 있는 자료구조를 말합니다.
    명령어 수신 탑(높이)
    I 숫자 큐에 주어진 숫자를 삽입합니다.
    D 1 큐에서 최댓값을 삭제합니다.
    D -1 큐에서 최솟값을 삭제합니다.
    이중 우선순위 큐가 할 연산 operations가 매개변수로 주어질 때, 모든 연산을 처리한 후 큐가 비어있으면 [0,0] 비어있지 않으면 [최댓값, 최솟값]을 return 하도록 solution 함수를 구현해주세요.

--- test_0527_1.py
+++ test_0527_1.py
@@ -1,29 +1,3 @@
-# def solution(jobs):
-#     import heapq
-#     n = len(jobs)
-#     time, end, heap = 0, -1, []
-#     answer, count = 0, 0
-#     while count < n:
-#         for i in jobs:
-#             if end < i[0] <= time:
-#                 answer += (time - i[0])
-#                 heapq.heappush(heap, i[1])
-#         if len(heap) > 0:
-#             answer += len(heap) * heap[0]
-#             end = time
-#             time += heapq.heappop(heap)
-#             count += 1
-#         else:
-#             time += 1
-#
-#     return (int(answer / n))
-#
-# jobs = [[0, 3], [1, 9], [2, 6]]
-# print(solution(jobs))
-
-
-
-
 def solution(operations):
     import heapq
     answer = []
@@ -34,39 +8,22 @@ def solution(operations):
         item.append(operations[i].split(" "))
     while count != len(operations):
         if (item[num][0] == 'I'):
-            heapq.heappush(heap, item[num][1])
+            heapq.heappush(heap, int(item[num][1]))
         elif (item[num][0] == 'D'):
             if(len(heap) != 0):
                 if (item[num][1] == '1'):
-                    heapq.heappop(heap)[-1]
+                    heap.sort(reverse=True)
+                    heapq.heappop(heap)
                 elif (item[num][1] == '-1'):
+                    heap.sort()
                     heapq.heappop(heap)
         num += 1
         count += 1
-    answer = [0, 0]
     if (heap != []):
-        answer = heap
+        heap.sort(reverse=True)
+        answer.append(heapq.heappop(heap))
+        heap.sort()
+        answer.append(heapq.heappop(heap))
+    else :
+        answer = [0, 0]
     return answer
-
-# operations = ["I 16","I -5643", "D -1", "D 1", "I 123", "D -1"]
-# print(solution(operations))
-# operations = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
-# print(solution(operations))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
