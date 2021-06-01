e118b5e8b1e9a01c9113fafe8796286f7ea47113
Dogyu <hello2263@naver.com>
2021-06-01 13:44:07 +0900

    프로그래머스 완전탐색 > 소수찾기
    한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
    각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
    제한사항
    numbers는 길이 1 이상 7 이하인 문자열입니다.
    numbers는 0~9까지 숫자만으로 이루어져 있습니다.
    "013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.

--- test_0601_1.py
+++ test_0601_1.py
@@ -1,23 +1,24 @@
 def solution(numbers):
     from itertools import permutations
     import math
-    import pandas as pd
     answer = 0
     count = []
+    flag = 0
     for i in range(1, len(numbers)+1):
         count.extend(list(int(''.join(i)) for i in permutations(numbers, i)))
-    count = pd.unique(count).tolist()
+    t_s = set(count)
+    t_l = list(t_s)
+    count = t_l
     for j in count:
         num = math.sqrt(j)
         for k in range(2, int(num)+1):
             if j % k == 0:
+                flag = 0
                 break
-        if (j >= 2):
+        if ((j >= 2)&(flag == 1)) == 1:
             answer += 1
-    print(count)
+        elif (j==2):
+            answer += 1
+        flag = 1
     return answer
 
-numbers = "17"
-print(solution((numbers)))
-numbers = "011"
-print(solution((numbers)))
